49844670aeba4bba0c2cb2703f133feccbda6ff6
TC1050 <denune.muhammad.ft21@mail.umy.ac.id>
2023-11-16 00:01:59 +0700

    Menambahkkan value berupa nama, alamat, dan telepon pada data class

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.esjumbo"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.esjumbo"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
